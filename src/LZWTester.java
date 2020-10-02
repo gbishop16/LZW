afea6835973df6e2644429a523c927af554ecba3
gbishop16 <gqbishop@gmail.com>
2020-10-02 16:42:59 -0700

    Standardizing changes
    Making changes tot he files to make them fit the format set by the class. Added ascii art, ms kaufman code, character limit.

--- src/LZWTester.java
+++ src/LZWTester.java
@@ -1,11 +1,26 @@
+
+/*
+ * Praise be to Ms. Kaufman and Computer Science A teachers.
+ * They spoke the truth when they spoke of handwritten code and BlueJ. 
+ */
 import java.io.*;
 import java.util.*;
+
 public class LZWTester {
-	public static void main(String[] args) throws IOException
-	{
+	public static void main(String[] args) throws IOException {
 //		Encode testEncoder = new Encode();
 //		testEncoder.encoding("file1.txt");
 		Decode testDecoder = new Decode();
 		testDecoder.decode("file1.txt.lzw");
 	}
+	/*
+	 *  _..._
+      .'     '.
+     / \     / \
+    (  |     |  )
+    (`"`  "  `"`)
+     \         /
+      \  ___  /
+       '.___.'
+	 */
 }
